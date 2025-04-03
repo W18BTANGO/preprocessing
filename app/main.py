7d534c513516d1a8760a0293e66df664cf46a9b7
Abbie Worswick <abbieworswick8@gmail.com>
2025-04-03 13:05:02 +1100

    fixed cors

--- app/main.py
+++ app/main.py
@@ -8,6 +8,10 @@ app = FastAPI(title="Preprocessing API", description="API for extracting specifi
 
 app.add_middleware(
     CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
 )
 
 
